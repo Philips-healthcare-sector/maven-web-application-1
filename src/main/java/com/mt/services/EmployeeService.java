2ef0108904ef9feb6e1da15b985b77ceaa240774
vinaysgithub <121040530+vinaysgithub@users.noreply.github.com>
2023-01-03 13:44:27 +0530

    Update EmployeeService.java

--- src/main/java/com/mt/services/EmployeeService.java
+++ src/main/java/com/mt/services/EmployeeService.java
@@ -22,11 +22,11 @@ public class EmployeeService {
 			throws JSONException {
 
 		JSONObject js = new JSONObject();
-		js.put("Name", "Mithun Technologies");
-		js.put("Calling Name", "Mithun");
-		js.put("DOB", "08-Nov-2011");
-		js.put("Hobbies", "Reading Technical Blogs,Teaching, Helping to Poor People..");
-		js.put("Places he like", "His native place");
+		js.put("Name", "vinay yenaganti");
+		js.put("Calling Name", "vinay");
+		js.put("DOB", "14-Dec-1991");
+		js.put("Hobbies", "Reading books, watching movies..");
+		js.put("Places he like", "vizag,dehradun");
 
 		return js.toString();
 }
